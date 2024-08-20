7e77d129bb81cb3297af293224f2f7f8facf2ea1
melitafreiberga <160214046+melitafreiberga@users.noreply.github.com>
2024-08-20 12:52:18 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
+from my_package.image_processing import DocumentProcessingApp
+from my_package.drive_uploader import DriveUploaderApp
 
+
+# Main logic with user choice
+def main():
+    # PROCESS THE IMAGE
+    DocumentProcessingApp().run()
+
+    # SAVE FILE ON GOOGLE DRIVE
+    DriveUploaderApp().run()
+
+
+if __name__ == "__main__":
+    main()
