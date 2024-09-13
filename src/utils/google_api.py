477d34870db6587d34ef93fca9f08ee37ded4b90
melitafreiberga <160214046+melitafreiberga@users.noreply.github.com>
2024-09-13 20:07:41 +0200

    Update google_api.py

--- src/utils/google_api.py
+++ src/utils/google_api.py
@@ -83,6 +83,7 @@ def choose_folder(service, status_label):
     :return: The list of folders.
     """
     folders = list_folders(service)  # Get the list of folders from Google Drive
+    '''
     if folders:
         # Prompt the user in the GUI
         status_label.text = "Folders found. Select one by entering its ID in the input field."
@@ -91,6 +92,7 @@ def choose_folder(service, status_label):
     else:
         # Inform the user if no folders are found
         status_label.text = "No folders found, file will be uploaded to the root directory."
+    '''
     return folders
 
 
